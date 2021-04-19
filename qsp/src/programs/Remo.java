727b28dc1c8233f8d2380a2146ca4fe94107ccf3
pappot1234 <82838544+pappot1234@users.noreply.github.com>
2021-04-20 01:26:30 +0530

    Update Remo.java

--- qsp/src/programs/Remo.java
+++ qsp/src/programs/Remo.java
@@ -4,7 +4,7 @@ public class Remo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi I am from remo class");
+		System.out.println("Hi I am from remo class and i have undergone some changes in github");
 
 	}
 
